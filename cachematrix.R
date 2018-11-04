468a364963db6c2dd6405dac82394ac13d59d940
ElenaSpasova <43963812+ElenaSpasova@users.noreply.github.com>
2018-11-04 20:48:03 +0200

    Update cachematrix.R
    Solution to programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to provide functionality to calculate inverted matrices and to cache them for later, faster access. 
 
-## Write a short comment describing this function
+## The function "makeCacheMatrix" creates an object with two properties: one for the orginal matrix, and one - for the inverted matrix.  Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverted_matrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inverted_matrix <<- NULL
+  }
+  get <- function() x
+  setinverted <- function(new_inverted_matrix) inverted_matrix <<- new_inverted_matrix
+  getinverted <- function() inverted_matrix
+  list(set = set, get = get,
+       setinverted = setinverted,
+       getinverted = getinverted)
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve" is a function retrieving the cached inverted matrix (in case there is one) 
+## or calculates the inverted matrix and caches it (in that case the message "calculating new inverted matrix" is shown). 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverted_matrix <- x$getinverted()
+  if(!is.null(inverted_matrix)) {
+    message("getting cached data")
+    return(inverted_matrix)
+  }
+  message("calculating new inverted matrix")
+  data <- x$get()
+  inverted_matrix <- solve(data, ...)
+  x$setinverted(inverted_matrix)
+  inverted_matrix
 }
